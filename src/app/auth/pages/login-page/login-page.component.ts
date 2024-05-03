ec83420edb8ff1ae5d006b5cc89855ddafa50614
Arseniy Fedarynchyk <1995arseny@gmail.com>
2024-05-03 11:55:24 +0200

    Create template for login form

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
+  readonly authForm = this.fb.nonNullable.group({
+    login: ['', Validators.required],
+    password: ['', Validators.required],
+  });
 
+  constructor(private readonly fb: FormBuilder) {}
+
+  onSubmit(): void {
+    console.log('Submitted!');
+  }
 }
