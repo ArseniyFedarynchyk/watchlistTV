e726bbe91eb786dd4d719257888b0c98a615067d
Arseniy Fedarynchyk <1995arseny@gmail.com>
2024-07-01 10:29:32 +0200

    Create logout functionality

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import {
+  Auth,
+  signInWithEmailAndPassword,
+  user,
+  signOut,
+} from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { Observable, from } from 'rxjs';
 
@@ -7,17 +12,24 @@ import { Observable, from } from 'rxjs';
   providedIn: 'root',
 })
 export class AuthService {
+  user$ = user(this.fireAuth);
+
   constructor(
-    private readonly fireAtuh: Auth,
+    private readonly fireAuth: Auth,
     private readonly router: Router
   ) {}
 
   login(email: string, password: string): Observable<void> {
     const promise = signInWithEmailAndPassword(
-      this.fireAtuh,
+      this.fireAuth,
       email,
       password
     ).then(() => {});
+    return from(promise);
+  }
+
+  logout(): Observable<void> {
+    const promise = signOut(this.fireAuth);
     return from(promise);
   }
 }
