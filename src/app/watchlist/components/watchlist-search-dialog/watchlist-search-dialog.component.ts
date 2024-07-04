a2a06718148ddc8acf114efdefb44cd49f1dca5a
Arseniy Fedarynchyk <1995arseny@gmail.com>
2024-07-04 09:29:22 +0200

    Create form group and form control for the search field

--- src/app/watchlist/components/watchlist-search-dialog/watchlist-search-dialog.component.ts
+++ src/app/watchlist/components/watchlist-search-dialog/watchlist-search-dialog.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-watchlist-search-dialog',
   standalone: true,
-  imports: [MatInputModule, MatFormFieldModule],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './watchlist-search-dialog.component.html',
   styleUrl: './watchlist-search-dialog.component.scss',
 })
-export class WatchlistSearchDialogComponent {}
+export class WatchlistSearchDialogComponent {
+  searchForm = this.fb.nonNullable.group({
+    search: [''],
+  });
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  onSubmit(): void {
+    console.log('submit was clicked!');
+  }
+}
