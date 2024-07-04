3dd97cf9d57ef37abe041e0dbee8fe1daeafb93f
Arseniy Fedarynchyk <1995arseny@gmail.com>
2024-07-04 09:11:53 +0200

    Create watchlist components with an input to search a movie

--- src/app/watchlist/components/watchlist-search-dialog/watchlist-search-dialog.component.ts
+++ src/app/watchlist/components/watchlist-search-dialog/watchlist-search-dialog.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-watchlist-search-dialog',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatFormFieldModule],
   templateUrl: './watchlist-search-dialog.component.html',
-  styleUrl: './watchlist-search-dialog.component.scss'
+  styleUrl: './watchlist-search-dialog.component.scss',
 })
-export class WatchlistSearchDialogComponent {
-
-}
+export class WatchlistSearchDialogComponent {}
