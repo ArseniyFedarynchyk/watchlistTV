1865b84314674c7c3f7c36d668fc0d2b1658351c
Arseniy Fedarynchyk <1995arseny@gmail.com>
2024-07-16 14:17:26 +0200

    Use watchlist-collection component in the watchlist page

--- src/app/watchlist/pages/watchlist-page/watchlist-page.component.ts
+++ src/app/watchlist/pages/watchlist-page/watchlist-page.component.ts
@@ -1,16 +1,24 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { WatchlistSearchDialogComponent } from '../../components/watchlist-search-dialog/watchlist-search-dialog.component';
 import { WatchlistPageStore } from './watchlist-page.store';
+import { WatchlistCollectionComponent } from '../../components/watchlist-collection/watchlist-collection.component';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-watchlist-page',
   standalone: true,
-  imports: [WatchlistSearchDialogComponent],
+  imports: [
+    WatchlistSearchDialogComponent,
+    WatchlistCollectionComponent,
+    AsyncPipe,
+  ],
   templateUrl: './watchlist-page.component.html',
   styleUrl: './watchlist-page.component.scss',
   providers: [WatchlistPageStore],
 })
 export class WatchlistPageComponent implements OnInit {
+  readonly movies$ = this.watchlistPageStore.movies$;
+
   constructor(private readonly watchlistPageStore: WatchlistPageStore) {}
 
   ngOnInit(): void {
