4520d5a8456701ccf4d54810039b53976477159d
Arseniy Fedarynchyk <1995arseny@gmail.com>
2024-08-10 08:24:06 +0200

    Add remove button to each movie card from users collection

--- src/app/watchlist/components/watchlist-collection/watchlist-collection.component.ts
+++ src/app/watchlist/components/watchlist-collection/watchlist-collection.component.ts
@@ -1,16 +1,27 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { Movie } from '../../models/movie.model';
 import { MatIconModule } from '@angular/material/icon';
 import { NgOptimizedImage } from '@angular/common';
 import { MovieCardComponent } from '../movie-card/movie-card.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-watchlist-collection',
   standalone: true,
-  imports: [MatIconModule, NgOptimizedImage, MovieCardComponent],
+  imports: [
+    MatIconModule,
+    NgOptimizedImage,
+    MovieCardComponent,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './watchlist-collection.component.html',
   styleUrl: './watchlist-collection.component.scss',
 })
 export class WatchlistCollectionComponent {
   readonly movies = input.required<Movie[]>();
+
+  onDelete(): void {
+    console.log('Delete button was clicked!');
+  }
 }
