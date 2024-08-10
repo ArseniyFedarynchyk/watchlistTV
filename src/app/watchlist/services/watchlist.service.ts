05bdccf0203f1a25b1ba3385067cbae45ea291dc
Arseniy Fedarynchyk <1995arseny@gmail.com>
2024-08-10 08:34:25 +0200

    Create a method to post movies and update a method to get movies fro database to have an id of each element

--- src/app/watchlist/services/watchlist.service.ts
+++ src/app/watchlist/services/watchlist.service.ts
@@ -1,9 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, signal } from '@angular/core';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { ServerResponse } from '../models/server-response.model';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import {
+  Firestore,
+  addDoc,
+  collection,
+  collectionData,
+  deleteDoc,
+  doc,
+} from '@angular/fire/firestore';
 import { MovieAPI } from '../models/movie-api.model';
+import { Movie } from '../models/movie.model';
+import { MovieMapperService } from './movie-mapper.service';
 
 @Injectable({
   providedIn: 'root',
@@ -14,11 +23,20 @@ export class WatchListService {
 
   constructor(
     private readonly http: HttpClient,
-    private readonly firestore: Firestore
+    private readonly firestore: Firestore,
+    private readonly movieMapperService: MovieMapperService
   ) {}
 
   getMovies(): Observable<MovieAPI[]> {
-    return collectionData(this.movieCollection) as Observable<MovieAPI[]>;
+    return collectionData(this.movieCollection, {
+      idField: 'id',
+    }) as Observable<MovieAPI[]>;
+  }
+
+  postMovies(movie: Movie) {
+    const movieMapped = this.movieMapperService.mapMovieToAPIModel(movie);
+    const promise = addDoc(this.movieCollection, movieMapped);
+    return from(promise);
   }
 
   searchMovies(movie: string): Observable<ServerResponse> {
