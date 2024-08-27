67df01d280ea61beef7568668e63d218b4a65db8
Arseniy Fedarynchyk <1995arseny@gmail.com>
2024-08-27 12:56:51 +0200

    Use authentification guard to protect the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
 import { unAuthGuard } from './core/guards/un-auth.guard';
 import { WatchlistPageComponent } from './watchlist/pages/watchlist-page/watchlist-page.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginPageComponent, canActivate: [unAuthGuard] },
-  { path: '', component: WatchlistPageComponent },
+  { path: '', component: WatchlistPageComponent, canActivate: [authGuard] },
 ];
